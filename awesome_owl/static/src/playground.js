f51129da9016ea498a580063d38c34e78dc6cc9c
andman <andymansellspain@yahoo.es>
2024-09-08 09:19:03 +0200

    awesome_owl increment

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,7 +1,15 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { useState, Component } from "@odoo/owl";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
+
+    setup() {
+        this.state = useState({ value: 0 });
+    }
+
+    async increment() {
+        this.state.value++;
+    }
 }
