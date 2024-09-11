f3f7f1bf8baaae5653c049b768c3a4d05304e251
andman <andymansellspain@yahoo.es>
2024-09-11 19:33:53 +0200

    create counter object

--- awesome_owl/static/src/playground.js
+++ awesome_owl/static/src/playground.js
@@ -1,15 +1,9 @@
 /** @odoo-module **/
 
-import { useState, Component } from "@odoo/owl";
+import { Component } from "@odoo/owl";
+import { Counter } from "./counter/counter";
 
 export class Playground extends Component {
     static template = "awesome_owl.playground";
-
-    setup() {
-        this.state = useState({ value: 0 });
-    }
-
-    async increment() {
-        this.state.value++;
-    }
+    static components = { Counter };
 }
